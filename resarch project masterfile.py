eba777512722c7dd3f12a1e2f2415c50f7c03c1f
ivy <ivy.j.thorne@gmail.com>
2025-07-08 15:50:48 -0700

    Update resarch project masterfile.py

--- resarch project masterfile.py
+++ resarch project masterfile.py
@@ -154,6 +154,7 @@ def main():
     #dataBasePrinter("responses")
     for row in responsescurs.execute("SELECT * FROM Responses WHERE model_name = 'gemini-2.5-flash'"):
         print(row)
+        print("\n")
 
 
 
